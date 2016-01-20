c8a71f927c839f81a9c83d1dfa6ea3da86a40fb1
davidwhogg <david.hogg@nyu.edu>
2016-01-19 20:21:38 -0500

    data munging

--- code/get_data.py
+++ code/get_data.py
@@ -11,18 +11,25 @@ star = client.star(8462852)
 lcs = star.get_light_curves()
 
 # Loop over the datasets and read in the data.
-time, flux, ferr, quality = [], [], [], []
+time, flux, ivar, quality = [], [], [], []
 for lc in lcs:
     with lc.open() as f:
         # The lightcurve data are in the first FITS HDU.
         hdu_data = f[1].data
         time = np.append(time, hdu_data["time"])
-        flux = np.append(flux, hdu_data["sap_flux"])
-        ferr = np.append(ferr, hdu_data["sap_flux_err"])
+        thisflux = hdu_data["pdcsap_flux"]
+        good = np.isfinite(thisflux)
+        bad = np.logical_not(good)
+        median = np.median(thisflux[good])
+        thisflux /= median
+        thisflux[bad] = 1.
+        flux = np.append(flux, thisflux)
+        thisferr = hdu_data["pdcsap_flux_err"] / median
+        thisferr[bad] = np.Inf
+        ivar = np.append(ivar, 1. / (thisferr * thisferr))
         quality.append(hdu_data["sap_quality"])
-ivar = 1. / (ferr ** 2)
 
-print(time.shape, flux.shape)
+print(time.shape, flux.shape, ivar.shape, np.median(flux), np.median(ivar))
 
 plt.clf()
 plt.plot(time, flux, "k.", alpha=0.25)
